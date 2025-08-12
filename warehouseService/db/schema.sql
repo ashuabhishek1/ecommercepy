73292ac9782ddfb6240d5d649f9b37653bedad07
ashuabhishek1 <43219020+ashuabhishek1@users.noreply.github.com>
2025-08-12 19:05:57 +0530

    added wstock new field

--- warehouseService/db/schema.sql
+++ warehouseService/db/schema.sql
@@ -2,5 +2,6 @@ CREATE SCHEMA IF NOT EXISTS warehouse;
 CREATE TABLE IF NOT EXISTS warehouse.stock (
     sku TEXT PRIMARY KEY,
     location TEXT NOT NULL DEFAULT 'MAIN',
-    quantity INTEGER NOT NULL CHECK(quantity>=0)
+    quantity INTEGER NOT NULL CHECK(quantity>=0),
+    wcode TEXT NOT NULL
 );
